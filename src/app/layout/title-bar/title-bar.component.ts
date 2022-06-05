a7ea995b672539f9d76bcdf8c11ba1c859c3fe6e
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-06-04 18:09:15 -0600

    setup title bar

--- src/app/layout/title-bar/title-bar.component.ts
+++ src/app/layout/title-bar/title-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AppService } from 'src/app/services/app.service';
 
 @Component({
@@ -8,17 +9,42 @@ import { AppService } from 'src/app/services/app.service';
 })
 export class TitleBarComponent implements OnInit {
   public sidebarOpen : boolean = true;
+  public url:any = '';
 
-  constructor(private appService : AppService) { }
+  constructor(private appService : AppService, private router: Router) { }
 
   ngOnInit(): void {
     this.appService.sidebarSubject$.subscribe((isOpen:boolean)=>{
       this.sidebarOpen = isOpen;
-    })
+    });
+
+    // detech route changes
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+          // get current route
+          this.url = event?.url;
+      }
+    });
   }
   
   setOpen(bool:boolean){
     this.appService.sidebarSubject.next(bool);
   }
 
+  getTitleName(){
+    if(this.url === "/dashboard"){
+      return "Dashboard";
+    }
+    else if(this.url === "/invoices"){
+      return "Invoices";
+    }
+    else if(this.url === "/feedback"){
+      return "Feedback";
+    }
+    else if(this.url === "/jobs"){
+      return "Jobs"
+    }
+    return "";
+  }
+ 
 }
