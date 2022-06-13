7a2473637d395a447e2c8a4f19af90f08efbdcdd
vikas583 <aroravikas583@gmail.com>
2022-06-13 23:51:27 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,26 @@ import { InvoicesComponent } from './pages/invoices/invoices.component';
 import { JobsComponent } from './pages/jobs/jobs.component';
 
 const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent,children:[
-    {path:'recent',component:JobListComponent},
-    {path:'matches',component:JobListComponent},
-    {path:'saved',component:JobListComponent},
-    {path:'',redirectTo:'recent',pathMatch:'full'}
-  ]},
-  {path:'invoices',component:InvoicesComponent},
-  {path:'jobs',component:JobsComponent},
-  {path:'feedback',component:FeedbackComponent},
-  {path:'',redirectTo:'dashboard',pathMatch:'full'}
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      { path: 'recent', component: JobListComponent },
+      { path: 'matches', component: JobListComponent },
+      { path: 'saved', component: JobListComponent },
+      { path: '', redirectTo: 'recent', pathMatch: 'full' }
+    ]
+  },
+  { path: 'invoices', component: InvoicesComponent },
+  {
+    path: 'jobs', component: JobsComponent
+  },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
